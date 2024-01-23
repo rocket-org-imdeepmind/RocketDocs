c65d3a783695f82958bfc04efaf11bbd95e2ce70
Abhishek Chatterjee <abhishek@imdeepmind.com>
2024-01-23 17:45:12 +0530

    added the project

---
+++ docusaurus.config.js
@@ -0,0 +1,131 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Rocket: Create Backend APIs With Ease',
+  tagline: 'Documentation for Rocket',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://help.rocketapi.net',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js')
+        },
+        blog: {
+          showReadingTime: true
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      colorMode: {
+        defaultMode: "dark",
+        disableSwitch: false,
+      },
+      navbar: {
+        title: 'Rocket',
+        logo: {
+          alt: 'Rocket Logo',
+          src: 'img/rocket.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Docs',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Get Started',
+                to: '/docs/get-started/introduction',
+              },
+              {
+                label: 'Database',
+                to: '/docs/databases/introduction',
+              },
+              {
+                label: 'Table',
+                to: '/docs/tables/introduction',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'LinkedIn',
+                href: 'https://www.linkedin.com/company/imdeepmind-org',
+              },
+              {
+                label: 'Community',
+                href: 'https://github.com/imdeepmind/Rocket/discussions',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/imdeepmind',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/imdeepmind/Rocket',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Abhishek Chatterjee , Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
